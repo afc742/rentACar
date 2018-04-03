06241a3152423c88cd22bd0b62acfaa2aaf95efd
unknown <mjp151@uowmail.edu.au>
2018-04-03 14:26:41 +1000

    Added link to about page from welcome

--- project/resources/views/pages/index.blade.php
+++ project/resources/views/pages/index.blade.php
@@ -5,5 +5,6 @@
         <h1>{{$title}}</h1>
         <p>Give us ya car and we'll give it to a stranger</p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-info btn-lg" href="/register" role="button">Register</a></p>
+        <p><a href="/about">About</a></p>
 @endsection
 
