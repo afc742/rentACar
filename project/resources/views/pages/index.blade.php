65cbb6cdcde7e28f70be3ec7b7cec8be5f8989b1
unknown <fc.alec@gmail.com>
2018-04-10 13:57:00 +1000

    added if/else statment to index, if logged in/not logged in

--- project/resources/views/pages/index.blade.php
+++ project/resources/views/pages/index.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
-
-@section('content')
-    <div class="jumbotron text-center">
-        <h1>{{$title}}</h1>
-        <p>Give us ya car and we'll give it to a stranger</p>
-        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-info btn-lg" href="/register" role="button">Register</a></p>
+@if (Route::has('login'))
+@auth
+@section('content')  
+            <h1>"LOGGED IN"</h1>
 @endsection
+@else
+@section('content')  
+        <div class="jumbotron text-center">
+            <h1>{{$title}}</h1>
+            <p>Give us ya car and we'll give it to a stranger</p>
+            <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-info btn-lg" href="/register" role="button">Register</a></p>    
+@endsection
+@endauth
+@endif
+
 
