0b874f8f651b08137da66b3df1f6f17185cd0fb4
unknown <mjp151@uowmail.edu.au>
2018-05-17 16:35:11 +1000

    added about information

--- project/resources/views/pages/index.blade.php
+++ project/resources/views/pages/index.blade.php
@@ -17,9 +17,21 @@
         <br>
         <p>Hmmm, why give car?</p2>
 	</div>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <p> hi <p>
+    <br><br>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <p>CarApp is a great alternative to owning a car. And as a CarApp member you can use a wide selection of strangers cars, 
+                    as and when you need to and only pay for what you use.</p>
+                    <p>Just select a car and required time and lock it in, we will then send you a pickup location and code to access the car, 
+                    simple as that!</p>
+                    <p>This is a great asset to utilise in a situation where you need a vehicle short-term, saving you the hassle and cost of per-day 
+                    rental. Take advantage of the convenience and simplicity and you're sure to love it.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @endauth
 @endif
